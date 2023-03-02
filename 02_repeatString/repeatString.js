132d1b00871bc8893f9954f33b157ad72ba96c2a
Beam Sanctus <beam.sanctus@gmail.com>
2023-03-02 22:46:02 +0700

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function ( word, count) { 
+    if (count >= 0) {
+        return word.repeat(count);
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
