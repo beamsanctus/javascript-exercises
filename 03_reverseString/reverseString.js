b9b7265e12ba1ee947e4156fbc30df970d696497
Beam Sanctus <beam.sanctus@gmail.com>
2023-03-02 22:53:43 +0700

    03 Done but not optimize

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    let letters = word.split('');
+    let newWord = ''
+    for (let i = letters.length-1; i >= 0; i--) {
+        newWord += letters[i];
+    }
+    return newWord;
 };
 
 // Do not edit below this line
