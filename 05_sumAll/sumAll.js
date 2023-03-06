8472ec9c277087915ecb0b459904ed7feb6d792a
Beam Sanctus <beam.sanctus@gmail.com>
2023-03-06 15:00:36 +0700

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    if (typeof (x) != "number" || typeof (y) != "number" || x < 0 || y < 0) {
+        return 'ERROR';
+    } else {
+        let a = x < y ? x : y;
+        let b = y > x ? y : x;
+        let result = a;
+        for (let i = a + 1; i <= b; i++) {
+            result += i;
+        }
+        return result;
+    }
 };
+
+
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
