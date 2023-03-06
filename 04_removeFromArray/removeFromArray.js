bd160a8c126fd840d29362955686de35297f858b
Beam Sanctus <beam.sanctus@gmail.com>
2023-03-06 14:42:36 +0700

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+    let arrArg = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        let index = arrArg.indexOf(arguments[i]);
+        console.log(arguments[i] + '= index(' + index + ')');
+        if (index >= 0)
+            arrArg.splice(index, 1);
+        console.log(arrArg);
+    }
+    return arrArg;
 };
+
+
+//removeFromArray([1, 2, 3, 4], 7, "tacos");
 
 // Do not edit below this line
 module.exports = removeFromArray;
