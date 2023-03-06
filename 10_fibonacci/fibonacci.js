d0a2f1f49e5f81a960ab3de5c669d224f12e7293
Beam Sanctus <beam.sanctus@gmail.com>
2023-03-06 16:15:49 +0700

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    result = 0;
+    preNum1 = 1;
+    preNum2 = 1;
+    if (n <= 0) {
+        return "OOPS";
+    } else {
+        if (n == 1 || n == 2) {
+            result = 1;
+        } else {
+            for (let i = 3; i <= n; i++) {
+                result = preNum1 + preNum2;
+                preNum1 = preNum2;
+                preNum2 = result;
+            }
+        }
+    }
+    return result;
 };
 
+console.log(fibonacci("10"))
 // Do not edit below this line
 module.exports = fibonacci;
