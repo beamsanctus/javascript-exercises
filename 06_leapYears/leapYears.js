d3a399ebbd84b482aa1b4ee30a28f32728a5a1a1
Beam Sanctus <beam.sanctus@gmail.com>
2023-03-06 15:10:30 +0700

    06 Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let flag = false;
+    if (year % 4 === 0 ){
+        flag = true;
+    }
+    if (year % 100 === 0 ){
+        flag = false;
+    }
+    if (year % 400 === 0 ) {
+        flag = true;
+    }
+    return flag;
 };
+
+let year = 1996
+console.log(year)
+console.log(leapYears(year))
 
 // Do not edit below this line
 module.exports = leapYears;
