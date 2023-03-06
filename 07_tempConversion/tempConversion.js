8494ad31a420658be23d9e35d1ac557e103a750e
Beam Sanctus <beam.sanctus@gmail.com>
2023-03-06 15:19:34 +0700

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  if (typeof (fahrenheit) === "number") {
+    return Math.round((fahrenheit - 32) / 1.8 * 10) / 10;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  if (typeof (celsius) === "number") {
+    return Math.round((32 + (celsius * 9 / 5)) * 10) / 10;
+  }
 };
+
+console.log(convertToFahrenheit(-10))
+console.log(convertToCelsius(-100))
 
 // Do not edit below this line
 module.exports = {
