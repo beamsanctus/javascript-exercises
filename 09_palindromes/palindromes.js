06db21e1cd7a504da176f3b399e7564a2c47b636
Beam Sanctus <beam.sanctus@gmail.com>
2023-03-06 16:01:50 +0700

    09 done worse solution T.T

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let arr = word.toLowerCase().match(/[A-Za-z]/g);
+    let arr1 = arr.slice(0, Math.floor(arr.length / 2))
+    let arr2 = arr.slice(Math.floor((arr.length + 1) / 2)).reverse()
+    flag = true;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] != arr2[i]) {
+            flag = false;
+        }
+    }
+    return flag;
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
